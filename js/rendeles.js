c9e72514151f84153364615f4e26db3526d4292d
Sani <gori108@gmail.com>
2020-06-15 15:21:35 +0200

    j

---
+++ js/rendeles.js
@@ -0,0 +1,9 @@
+
+function calc() {
+    let amount = document.querySelector(".amount");
+    let price = 1200;
+    let ff = parseInt(amount.value) * price;
+    alert("Fizetendő összeg= " + ff);
+}
+
+//  //
