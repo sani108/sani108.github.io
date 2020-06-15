cbbde7f6a40903907cd6b078c420748a14ea9222
Sani <gori108@gmail.com>
2020-06-15 17:46:30 +0200

    validate

--- js/rendeles.js
+++ js/rendeles.js
@@ -2,8 +2,38 @@
 function calc() {
     let amount = document.querySelector(".amount");
     let price = 1200;
-    let ff = parseInt(amount.value) * price;
-    alert("Fizetendő összeg= " + ff);
+    let ff = parseInt(amount.value.trim()) * price;                 //trim kiveszi a szóközöket //
+    let ShowAmont = document.querySelector("span.ShowAmount");
+  ff = isNaN( ff) ? 0 : ff ; // Ha ShowAmont nem értelmezhető, akkor 0 legyen, ha értelmezhető, akkor maradjon az értéke  // 
+    if (ff>12000) { alert ("Kérjük max 10 db-ot rendeljen"); return;}     //return megszakítja a függvényt//
+    if (ff<1200) { alert ("Kérjük min 1 pizzát rendeljen") ; return;} 
+
+   // alert("Fizetendő összeg= " + ff); //
+    ShowAmont.innerHTML = ff;                // a ShowAmount HTML elemnek megvátoztatja az értékét ff-re//
 }
 
-//  //
+function testemail() {
+     let levell = document.querySelector(".email");
+   if (levell.value.indexOf("@")<0 || levell.value.indexOf(".")<0) alert("Kérjük ellenőrizze az email címet"); 
+  
+}
+
+/* 
+let name = "Péter";
+name.toUpperCase();    PÉTER
+name.toLowerCase();    péter
+"DR" + name;
+let title = "dr ";
+title.copncat(name);     dr Péter
+
+Boolen(0)    eredmény false
+Boolen(121)  minden eredménye, ami nem 0 true
+
+0 == false  eredmény true
+0 === false eredménye false mert különböző típusúak
+
+ */
+
+ let test = "Hare Krsna";
+ test.indexOf("Hare");
+ test.replace("Hare" , "Krisna");
